6edbfae40a7b3f65c2ab7aa94075367c5da81ee6
iSkipPro <dellpcinternetid@gmail.com>
2026-05-13 12:25:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,22 +4,23 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-# Render ke environment variables se URI uthayega
+# Render ke Environment Variables se link uthayega
 MONGO_URI = os.environ.get("MONGO_URI")
 client = MongoClient(MONGO_URI)
-db = client.iSkipPro
+db = client.iSkipPro 
 
 @app.route('/check_device', methods=['POST'])
 def check_device():
     data = request.json
     serial = data.get('serial', '').strip().upper()
     
+    # Collection 'registered_devices' mein serial dhoondna
     device = db.registered_devices.find_one({"serial": serial})
     
     if device:
         return jsonify({
             "status": "registered", 
-            "message": "A12+ With Signal Supported"
+            "message": "Authorized! A12+ With Signal Supported."
         })
     else:
         return jsonify({
